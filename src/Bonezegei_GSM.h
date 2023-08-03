e88167640a3f4b4273130fcf5d5fc6f7c0db6d5d
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-08-03 20:33:10 +0800

    Update Bonezegei_GSM.h

--- src/Bonezegei_GSM.h
+++ src/Bonezegei_GSM.h
@@ -1,1 +1,26 @@
+/*
+  This Library is written for GSM
+  Author: Bonezegei (Jofel Batutay)
+  Date: August 2023 
+*/
+#ifndef _BONEZEGEI_GSM_H_
+#define _BONEZEGEI_GSM_H_
+#include <Arduino.h>
+
+class Bonezegei_GSM {
+public:
+
+  Bonezegei_GSM();
+  Bonezegei_GSM(const HardwareSerial& se);
+
+  void begin();
+  void begin(int baud);
+  void sendMessage(const char *number,const char* message);
+
+private:
+  HardwareSerial *s;
+};
+
+
+#endif
 
