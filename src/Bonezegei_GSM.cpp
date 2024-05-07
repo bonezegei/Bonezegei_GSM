0d7249ac00d761b5fec6e97b36a08698bb2776ec
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-05-07 09:22:55 +0800

    Update Bonezegei_GSM.cpp

--- src/Bonezegei_GSM.cpp
+++ src/Bonezegei_GSM.cpp
@@ -10,26 +10,57 @@ Bonezegei_GSM::Bonezegei_GSM() {
 }
 
 Bonezegei_GSM::Bonezegei_GSM(const HardwareSerial& se) {
+  ser = 0;
   s = (HardwareSerial*)&se;
 }
 
+Bonezegei_GPS::Bonezegei_GSM(Bonezegei_SoftSerial& serial) {
+  ser = 1;
+  softserial = (Bonezegei_SoftSerial*)&serial;
+}
+
 void Bonezegei_GSM::begin() {
-  s->begin(9600);
+  if(ser==1){
+    softserial->begin(9600);
+  }
+  else{
+    s->begin(9600);
+  }
 }
 
 void Bonezegei_GSM::begin(int baud) {
-  s->begin(baud);
+  if(ser==1){
+    softserial->begin(baud);
+  }
+  else{
+    s->begin(baud);
+  }
 }
 
 void Bonezegei_GSM::sendMessage(const char *number,const char* message) {
-  s->println("AT+CMGF=1");
-  delay(1000);
-  s->print("AT+CMGS=\"");
-  s->print(number);
-  s->println("\"");
-  delay(800);
-  s->println(message);
-  delay(500);
-  s->println((char)26);
-  delay(5000);
+  if(ser==1){
+    softserial->println("AT+CMGF=1");
+    delay(1000);
+    softserial->print("AT+CMGS=\"");
+    softserial->print(number);
+    softserial->println("\"");
+    delay(800);
+    softserial->println(message);
+    delay(500);
+    softserial->println((char)26);
+    delay(5000);      
+  }
+  else{
+    s->println("AT+CMGF=1");
+    delay(1000);
+    s->print("AT+CMGS=\"");
+    s->print(number);
+    s->println("\"");
+    delay(800);
+    s->println(message);
+    delay(500);
+    s->println((char)26);
+    delay(5000);   
+  }
+
 }
